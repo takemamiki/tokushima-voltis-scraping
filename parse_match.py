d01edb5eba67ea35d734b6e13057a06c7214d683
TAKEMA MIKI <mikitakema@proton.me>
2026-09-06 20:52:50 +0900

    parse_match.pyをvortis.jp用に書き換え、関数化して動作確認

--- parse_match.py
+++ parse_match.py
@@ -1,6 +1,6 @@
 """
 parse_match.py
-fetch_match.py が取得したHTMLから、スコア・チーム名・得点者を抜き出す。
+fetch_match.py が取得したvortis.jpのHTMLから、スコア・チーム名・得点者を抜き出す。
 
 役割：HTMLの中から欲しい情報だけを見つけて取り出すこと。
     サイトにアクセスするのは fetch_match.py の役目。
@@ -8,41 +8,59 @@ fetch_match.py が取得したHTMLから、スコア・チーム名・得点者�
 
 from bs4 import BeautifulSoup
 
-with open("data/sample_match.html", "r", encoding="utf-8") as f:
-    html = f.read()
+def get_scorers_and_minutes(score_div):
+    """
+    homeScore/awayScoreのdivから、得点者名と得点時間のリストを取り出す。
+    無得点で<ul>が存在しない場合は空リストを返す。
+    """
+    scorers  = []
+    minutes = []
 
-soup = BeautifulSoup(html, "html.parser")
+    for li in score_div.find_all("li"):
+        minute_tag = li.find("span")
+        name_tag = li.find("span", class_="name")
 
-scores = soup.find_all("p", class_="o-page-header__match-score")
+        if minute_tag:
+            minutes.append(minute_tag.get_text(strip=True))
+        if name_tag:
+            scorers.append(name_tag.get_text(strip=True))
 
-home_score = scores[0].get_text(strip=True)
-away_score = scores[1].get_text(strip=True)
+    return scorers, minutes
 
-club_names = soup.find_all("p", class_="o-page-header__club-name", attrs={"data-media": "pc"})
-home_club = club_names[0].get_text(strip=True)
-away_club = club_names[1].get_text(strip=True)
 
-score_lists = soup.find_all("div", class_="o-page-header__player-score-list")
+def parse_match(html: str) -> dict:
+    """
+    試合結果ページのHTML文字列を受け取り、試合データを辞書で返す。
+    """
+    soup = BeautifulSoup(html, "html.parser")
 
-scorers = []
-minutes = []
-for score_list in score_lists:
-    player_links = score_list.find_all("a", class_="o-page-header__player-score-link")
-    for link in player_links:
-        scorers.append(link.get_text(strip=True))
+    home_club = soup.find("div", class_="home").find("figcaption").get_text(strip=True)
+    away_club = soup.find("div", class_="away").find("figcaption").get_text(strip=True)
 
-    score_p_tags = score_list.find_all("p", class_="a-typography--fw-semibold")
-    for tag in score_p_tags:
-        minutes.append(tag.get_text(strip=True))
+    home_score_div = soup.find("div", class_="homeScore")
+    away_score_div = soup.find("div", class_="awayScore")
 
-match_data = {
-    "home_club": home_club,
-    "away_club": away_club,
-    "home_score": home_score,
-    "away_score": away_score,
-    "scorers": scorers,
-    "minutes": minutes,
-}
+    home_score = home_score_div.find("span", recursive=False).get_text(strip=True)
+    away_score = away_score_div.find("span", recursive=False).get_text(strip=True)
 
-if __name__ == "__main__":
+    home_scorers, home_minutes = get_scorers_and_minutes(home_score_div)
+    away_scorers, away_minutes = get_scorers_and_minutes(away_score_div)
+
+    match_data = {
+        "home_club": home_club,
+        "away_club": away_club,
+        "home_score": home_score,
+        "away_score": away_score,
+        "scorers": home_scorers + away_scorers,
+        "minutes": home_minutes + away_minutes,
+        }
+
+    return match_data
+
+
+if __name__== "__main__":
+    with open("data/sample_vortis_match.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    
+    match_data = parse_match(html)
     print(match_data)
